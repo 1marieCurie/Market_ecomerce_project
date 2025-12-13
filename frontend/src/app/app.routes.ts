b37ca92b0fe77db1cce59ee1e2e49d8bb3ced609
unknown <khadija.oufquir22@gmail.com>
2025-12-14 00:33:26 +0100

    Frontend : Login done

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,15 +8,17 @@ import { AuthGuard } from '../guards/auth.guard';
 // Components 
 import { AdminComponent } from '../components/admin/admin.component'; 
 import { UserComponent } from '../components/user/user.component';
-//import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { HomeComponent } from '../components/home/home.component';
+//import { CartComponent } from '../components/cart/cart.component';
 
 export const routes: Routes = [
-    //{ path: '', component: HomeComponent }, // page publique
+    { path: '', component: HomeComponent }, // page publique
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     {path : 'admin', component :  AdminComponent, canActivate : [AdminGuard]},
     {path : 'user', component : UserComponent, canActivate: [AuthGuard] },
+   // { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: '' } // wildcard for unknown functions
 ]
